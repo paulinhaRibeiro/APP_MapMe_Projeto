631adc7d55fb10024b46cc8902f07aaf8af62f43
Paula Vitória <caped.20201p2ads0226@aluno.ifpi.edu.br>
2024-02-07 21:30:14 -0300

    Style: Tela de login
    Ajuste no layout da Tela de Login.

--- mapeme/lib/pages/authtentication/login_page.dart
+++ mapeme/lib/pages/authtentication/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mapeme/pagTeste.dart';
 
+
 class PageLogin extends StatefulWidget {
   const PageLogin({super.key});
 
@@ -26,7 +27,7 @@ class _PageLoginState extends State<PageLogin> {
       body: Center(
         child: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.all(15.0),
+            padding: const EdgeInsets.all(30.0),
             child: Form(
               key: formKey,
               child: Column(
@@ -38,139 +39,113 @@ class _PageLoginState extends State<PageLogin> {
                   ),
 
                   const SizedBox(
-                    height: 20,
+                    height: 40,
                   ),
                   // --------------- EMAIL -------------
-                  Container(
-                    margin: const EdgeInsets.all(8),
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-                    child: TextFormField(
-                      controller: emailController,
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return "Campo Obrigatório!";
-                        }
-                        return null;
-                      },
-                      decoration: InputDecoration(
-                        label: const Text("Email"),
-                        suffixIcon: const Icon(
-                          FontAwesomeIcons.envelope, //person
-                          size: 20,
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
+                  TextFormField(
+                    controller: emailController,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Campo Obrigatório!";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      label: const Text("Email"),
+                      suffixIcon: const Icon(
+                        FontAwesomeIcons.envelope, //person
+                        size: 20,
                       ),
-                    ),
-                  ),
-
-                  // --------------- SENHA -------------
-                  Container(
-                    margin: const EdgeInsets.all(8),
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-                    child: TextFormField(
-                      controller: passwordController,
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return "Campo Obrigatório!";
-                        }
-                        return null;
-                      },
-                      //visibilidade da senha
-                      obscureText: !isVisible,
-                      decoration: InputDecoration(
-                        label: const Text("Senha"),
-                        suffixIcon: IconButton(
-                          onPressed: () {
-                            // mudar o icone do botão do icone da senha
-                            // para alterar entre visivel e não visivel
-                            setState(() {
-                              isVisible = !isVisible;
-                            });
-                          },
-                          icon: Icon(
-                            isVisible
-                                ? FontAwesomeIcons.eye
-                                : FontAwesomeIcons.eyeSlash,
-                            size: 20,
-                          ),
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
                       ),
                     ),
                   ),
 
                   const SizedBox(
-                    height: 15,
+                    height: 20,
+                  ),
+
+                  // --------------- SENHA -------------
+                  TextFormField(
+                    controller: passwordController,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Campo Obrigatório!";
+                      }
+                      return null;
+                    },
+                    //visibilidade da senha
+                    obscureText: !isVisible,
+                    decoration: InputDecoration(
+                      label: const Text("Senha"),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          // mudar o icone do botão do icone da senha
+                          // para alterar entre visivel e não visivel
+                          setState(() {
+                            isVisible = !isVisible;
+                          });
+                        },
+                        icon: Icon(
+                          isVisible
+                              ? FontAwesomeIcons.eye
+                              : FontAwesomeIcons.eyeSlash,
+                          size: 20,
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(
+                    height: 30,
                   ),
 
                   // --------------- BOTÂO DE LOGIN -------------
-                  Container(
-                    height: 55,
-                    width: MediaQuery.of(context).size.width * .81,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (formKey.currentState!.validate()) {
-                          // metodod para o login
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const Teste()));
-                        }
-                        debugPrint("Email: ${emailController.text}");
-                        debugPrint("Senha: ${passwordController.text}");
-                      },
-                      style: ElevatedButton.styleFrom(
-                        // shape: RoundedRectangleBorder(
-                        //   borderRadius: BorderRadius.circular(10),),
-                        elevation: 10,
-                        minimumSize: const Size.fromHeight(40),
-                      ),
-                      child: const Text(
-                        "ENTRAR",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w700),
-                      ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        // metodod para o login
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Teste()));
+                      }
+                      debugPrint("Email: ${emailController.text}");
+                      debugPrint("Senha: ${passwordController.text}");
+                    },
+                    style: ElevatedButton.styleFrom(
+                      // shape: RoundedRectangleBorder(
+                      //   borderRadius: BorderRadius.circular(10),),
+                      elevation: 10,
+                      minimumSize: const Size.fromHeight(55),
+                    ),
+                    child: const Text(
+                      "Entrar",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
                     ),
                   ),
 
-                  Container(
-                    height: 55,
-                    width: MediaQuery.of(context).size.width * .85,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const Teste()));
-                          },
-                          child: const Text("Esqueceu a senha?"),
-                        ),
-                      ],
-                    ),
+                  const SizedBox(
+                    height: 5,
                   ),
 
+                  // --------------- Botão de esqueceu a senha -----------------
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      const Text("Não Tem cadastro?"),
                       TextButton(
                         onPressed: () {
-                          //navegar para a proxima página
                           Navigator.push(
                               context,
                               MaterialPageRoute(
                                   builder: (context) => const Teste()));
                         },
-                        child: const Text("Cadastre-se."),
+                        child: const Text("Esqueceu a senha?"),
                       ),
                     ],
                   ),
@@ -178,6 +153,31 @@ class _PageLoginState extends State<PageLogin> {
               ),
             ),
           ),
+        ),
+      ),
+
+      // ------------------- Botão de cadastro fixo na parte inferior da tela ------------------
+      bottomNavigationBar: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(vertical: 10.0),
+        // color: const Color.fromARGB(255, 255, 255, 255),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Não tem uma conta?",
+              // style: TextStyle(fontSize: 15),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Teste()),//PageSignUp()),
+                );
+              },
+              child: const Text("Cadastre-se."),
+            ),
+          ],
         ),
       ),
     );
